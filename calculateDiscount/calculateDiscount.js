7cdbded7454aad9696efa8b3b02e3b9ac2fe44b4
ashik190141 <ashanmahmudashik@gmail.com>
2024-05-23 20:04:55 +0600

    discount calculate

--- calculateDiscount/calculateDiscount.js
+++ calculateDiscount/calculateDiscount.js
@@ -1,32 +1,37 @@
 const { bookShop, school } = require ("../constant/discount");
 const discountModel = require("../module/discount/discount.model");
+const userDiscountModel = require("../module/userDiscount/userDiscount.model");
 
 const d = new Date();
 let month = d.getMonth();
 
 const calculateDiscount = async(allData, user) => {
     const result = [];
+    const userDiscountData = await userDiscountModel.find().exec();
+    let userDiscountInfo = userDiscountData[0]
 
     for (let i = 0; i < allData.length; i++) {
       let price = allData[i].price;
       let discountInfo = await discountModel.findOne({ productId : allData[i]._id});
-      let discountParentage =(price * (discountInfo?.discountParentage/100)) || 0
+      let discountParentage = (price * (discountInfo?.discountParentage / 100)) || 0;
       
       if (user.role == "school") {
         if (allData[i].category == "Pen") {
-          price = price - ((price * (school.pen / 100)) + discountParentage);
+          price = price - ((price * (userDiscountInfo.schoolPen / 100)) + discountParentage);
         } else if (allData[i].category == "Paper") {
-          price = price - ((price * (school.paper / 100)) + discountParentage);
+          price =
+            price -
+            (price * (userDiscountInfo.schoolPaper / 100) + discountParentage);
         } else {
-          price = price - ((price * (school.book / 100)) + discountParentage);
+          price = price - ((price * (userDiscountInfo.schoolBook / 100)) + discountParentage);
         }
       } else if (user.role == "bookshop") {
         if (allData[i].category == "Pen") {
-          price = price - ((price * (bookShop.pen / 100)) + discountParentage);
+          price = price - ((price * (userDiscountInfo.bookshopPen / 100)) + discountParentage);
         } else if (allData[i].category == "Paper") {
-          price = price - ((price * (bookShop.paper / 100)) + discountParentage);
+          price = price - ((price * (userDiscountInfo.bookshopPaper / 100)) + discountParentage);
         } else {
-          price = price - ((price * (bookShop.book[month] / 100)) + discountParentage);
+          price = price - ((price * (userDiscountInfo.bookshopBook[month] / 100)) + discountParentage);
         }
       }
 
--- calculateDiscount/calculateDiscountWithCart.js
+++ calculateDiscount/calculateDiscountWithCart.js
@@ -1,6 +1,7 @@
 const { bookShop, school } = require("../module/discount/discount.model");
 const discountModel = require("../module/discount/discount.model");
 const productModel = require("../module/product/product.model");
+const userDiscountModel = require("../module/userDiscount/userDiscount.model");
 
 const d = new Date();
 let month = d.getMonth();
@@ -8,6 +9,8 @@ let month = d.getMonth();
 const calculateDiscountOfCart = async (uniqueIDs, user, allData) => {
   let result = [];
   let totalPrice = 0;
+  const userDiscountData = await userDiscountModel.find().exec();
+  let userDiscountInfo = userDiscountData[0];
 
   for (let i = 0; i < uniqueIDs.length; i++) {
     let id = uniqueIDs[i];
@@ -22,23 +25,24 @@ const calculateDiscountOfCart = async (uniqueIDs, user, allData) => {
       price * (discountInfo?.discountParentage / 100) || 0;
 
     if (user.role == "school") {
-      if (allData[i].category == "Pen") {
-        price = price - (price * (school.pen / 100) + discountParentage);
-      } else if (allData[i].category == "Paper") {
-        price = price - (price * (school.paper / 100) + discountParentage);
-      } else {
-        price = price - (price * (school.book / 100) + discountParentage);
+        if (allData[i].category == "Pen") {
+          price = price - ((price * (userDiscountInfo.schoolPen / 100)) + discountParentage);
+        } else if (allData[i].category == "Paper") {
+          price =
+            price -
+            (price * (userDiscountInfo.schoolPaper / 100) + discountParentage);
+        } else {
+          price = price - ((price * (userDiscountInfo.schoolBook / 100)) + discountParentage);
+        }
+      } else if (user.role == "bookshop") {
+        if (allData[i].category == "Pen") {
+          price = price - ((price * (userDiscountInfo.bookshopPen / 100)) + discountParentage);
+        } else if (allData[i].category == "Paper") {
+          price = price - ((price * (userDiscountInfo.bookshopPaper / 100)) + discountParentage);
+        } else {
+          price = price - ((price * (userDiscountInfo.bookshopBook[month] / 100)) + discountParentage);
+        }
       }
-    } else if (user.role == "bookshop") {
-      if (allData[i].category == "Pen") {
-        price = price - (price * (bookShop.pen / 100) + discountParentage);
-      } else if (allData[i].category == "Paper") {
-        price = price - (price * (bookShop.paper / 100) + discountParentage);
-      } else {
-        price =
-          price - (price * (bookShop.book[month] / 100) + discountParentage);
-      }
-    }
       
     totalPrice = totalPrice + products.length * price;
 
